bccdbb4c04833a89c02a43b2d45be1d13fe3d7f9
realeyesJordan <jordan@realeyes.com>
2013-09-20 11:46:26 -0600

    Initial Commit
    Project setup and basic functionality.

---
+++ js/bootstrap.js
@@ -0,0 +1,9 @@
+// var $routeProvider = angular.module('app', ['ngRoute']);
+
+
+var ColorMatch = angular.module('ColorMatch', ['uiSlider', 'ngRoute', 'ColorMatch.GameModule']).config(['$routeProvider', function ($routeProvider) {
+	$routeProvider.when('/', {templateUrl: '/templates/base.tpl.html' });
+	console.log('Things fired.');
+
+
+}]);
---
+++ js/gameModule/gameModule.js
@@ -0,0 +1,52 @@
+var GameModule = angular.module('ColorMatch.GameModule', ['uiSlider','ngRoute' ]);
+
+GameModule.controller( 'GameModuleCtrl', ['$scope', '$element', '$attrs', function($scope, $element, $attrs) {
+
+	$scope.randomRedValue = 0;
+	$scope.randomGreenValue = 0;
+	$scope.randomBlueValue = 0;
+
+	$scope.userRedValue = 0;
+	$scope.userGreenValue = 0;
+	$scope.userBlueValue = 0;
+
+	$scope.startGame = function(value) {
+
+		// var random = Math.floor((Math.random()*255)+1);
+		// console.log('You clicked Start!', random);
+		$scope.randomRedValue = Math.floor((Math.random()*255)+1);Math.floor((Math.random()*255)+1);
+		$scope.randomGreenValue = Math.floor((Math.random()*255)+1);
+		$scope.randomBlueValue = Math.floor((Math.random()*255)+1);
+	}
+
+	$scope.submitGuess = function() {
+		// console.log('You clicked Submit!');
+		var redDiff = $scope.randomRedValue - $scope.userRedValue;
+		var blueDiff = $scope.randomGreenValue - $scope.userGreenValue;
+		var greenDiff = $scope.randomBlueValue - $scope.userBlueValue;
+		alert('you were this close - red: ' + redDiff + ' green: ' + greenDiff + ' blue: ' + blueDiff);
+	}
+
+}] );
+
+
+GameModule.directive('gameModule', [ function(){
+	// Runs during compile
+	return {
+		// name: '',
+		// priority: 1,
+		// terminal: true,
+		// scope: {}, // {} = isolate, true = child, false/undefined = no change
+		controller: 'GameModuleCtrl',
+		// require: 'ngModel', // Array = multiple requires, ? = optional, ^ = check parent elements
+		restrict: 'E', // E = Element, A = Attribute, C = Class, M = Comment
+		// template: '',
+		templateUrl: 'templates/game.tpl.html',
+		replace: true,
+		// transclude: true,
+		// compile: function(tElement, tAttrs, function transclude(function(scope, cloneLinkingFn){ return function linking(scope, elm, attrs){}})),
+		link: function($scope, iElm, iAttrs, controller) {
+			
+		}
+	};
+}]);
