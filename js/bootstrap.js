d7dde2b59079312ccb14eacba6fb698a5b21f10e
realeyesJordan <jordan@realeyes.com>
2013-09-20 18:29:28 -0600

    Updating src properties
    Still having issues with routes when app is not located at the root of
    the domain.

--- js/bootstrap.js
+++ js/bootstrap.js
@@ -2,7 +2,7 @@
 
 
 var ColorMatch = angular.module('ColorMatch', ['uiSlider', 'ngRoute', 'ColorMatch.GameModule']).config(['$routeProvider', function ($routeProvider) {
-	$routeProvider.when('/', {templateUrl: '/templates/base.tpl.html' });
+	$routeProvider.when('/', {templateUrl: 'templates/base.tpl.html' });
 	console.log('Things fired.');
 
 
